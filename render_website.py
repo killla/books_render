30ef64ff86c6a5173806ba2afea62251aaa2cd43
killla <killa.granet@gmail.com>
2020-05-20 22:27:57 +0500

    Fix 15-22 tasks

--- render_website.py
+++ render_website.py
@@ -2,8 +2,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server, shell
 from more_itertools import chunked
 from pathlib import Path
-import json, math, os
-from pprint import pprint
+import json, os
 
 
 env = Environment(
@@ -12,17 +11,14 @@ env = Environment(
 )
 
 
-def render_page(page_number, count_of_page, book_on_page, books):
+def render_page(page_number, count_of_pages, books):
     template = env.get_template('page_template.html')
-    begin = book_on_page * (page_number - 1)
-    end = book_on_page * page_number
-    books = books[begin:end]
     books = list(chunked(books, 2))
 
     prev_url, next_url = None, None
     if page_number > 1:
         prev_url = f'./index{page_number-1}.html'
-    if page_number < count_of_page:
+    if page_number < count_of_pages:
         next_url = f'./index{page_number+1}.html'
 
     rendered_page = template.render(
@@ -30,19 +26,19 @@ def render_page(page_number, count_of_page, book_on_page, books):
         prev_url=prev_url,
         next_url=next_url,
         page_number=page_number,
+        count_of_pages=count_of_pages,
     )
     page_filename = os.path.join('pages', f'index{page_number}.html')
 
     with open(page_filename, 'w', encoding="utf8") as file:
         file.write(rendered_page)
+    return page_filename
 
 
-def delete_old_pages(count_of_page):
-    for file in Path('.').glob('pages/index*.html'):
-        file_number = str(file)
-        file_number = int(file_number.replace('pages/index', '').replace('.html', ''))
-        if file_number > count_of_page:
-            os.remove(file)
+def delete_old_pages(paths):
+    for filename in Path('.').glob('pages/index*.html'):
+        if str(filename) not in paths:
+            os.remove(filename)
 
 
 def render_pages():
@@ -50,18 +46,21 @@ def render_pages():
 
     with open("books.json", "r") as books_file:
         books_json = books_file.read()
-    books_list = json.loads(books_json)
+    books = json.loads(books_json)
+    books_by_pages = list(chunked(books, book_on_page))
 
-    count_of_page = math.ceil(len(books_list)/book_on_page)
+    count_of_pages = len(books_by_pages)
+    paths_of_pages = []
 
-    for page_number in range(1, count_of_page+1):
-        render_page(page_number, count_of_page, book_on_page, books_list)
+    for page_number, books_on_current_page in enumerate(books_by_pages, 1):
+        page_path = render_page(page_number, count_of_pages, books_on_current_page)
+        paths_of_pages.append(page_path)
 
     template = env.get_template('main_page_template.html')
-    rendered_page = template.render(page_count = count_of_page)
+    rendered_page = template.render(page_count = count_of_pages)
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
-    delete_old_pages(count_of_page+1)
+    delete_old_pages(paths_of_pages)
 
 
 if __name__ =='__main__':
